6d50e0965fa8645fabd81d2d5b23c9abb55063ad
Yoshiki Kurihara <clouder@gmail.com>
2012-08-21 15:13:43 +0900

    Initial Commit

---
+++ YKUtility/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YKUtility
+//
+//  Created by 栗原 由樹 on 12/08/21.
+//  Copyright (c) 2012年 Yoshiki Kurihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YKUtility/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YKUtility
+//
+//  Created by 栗原 由樹 on 12/08/21.
+//  Copyright (c) 2012年 Yoshiki Kurihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
